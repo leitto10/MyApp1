51749390a17eae829b7c5ebb4f3424f2842fbfec
Jose Lopez <leitto10@gmail.com>
2019-04-21 02:44:21 -0700

    Buttons that display a toast message.

--- app/src/main/java/myapp1/com/MainActivity.java
+++ app/src/main/java/myapp1/com/MainActivity.java
@@ -5,15 +5,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /* Toast Method*/
+    public void toastMsg(String msg){
+        // creating messages for ech button
+        Toast a = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        a.show();
+        Toast b = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        b.show();
+        Toast c = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        c.show();
+        Toast d = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        d.show();
+    }
+
+    /* Method that call buttons messages*/
+    public void displayToastMsgA(View view){
+        toastMsg("Hi, this is Jose");
+    }
+    public void displayToastMsgB(View view){
+        toastMsg("Do you want to play a game?");
+    }
+    public void displayToastMsgC(View view){
+        toastMsg("My first Application");
+    }
+    public void displayToastMsgD(View view){
+        toastMsg("Everything goes well so far.");
     }
 
     /* Called when the User taps the send button*/
